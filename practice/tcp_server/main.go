b11e2b899b962a47b26d72112b61f892b1c93b2e
Siddhant Vij <siddhantvij201@gmail.com>
2024-04-11 19:54:35 +0530

    tcp server (http request response)

---
+++ practice/tcp_server/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	request(conn)
+	response(conn)
+}
+
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			method := strings.Fields(ln)[0]
+			url := strings.Fields(ln)[1]
+			fmt.Println("*****Method:", method, "*****URL:", url)
+		}
+		if ln == "" {
+			break
+		}
+		i++
+	}
+}
+
+func response(conn net.Conn) {
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>Hello World</strong></body></html>`
+
+	fmt.Println("HTTP/1.1 200 OK")
+	fmt.Printf("Content-Length: %d\n", len(body))
+	fmt.Println("Content-Type: text/html")
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
